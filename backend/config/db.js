09d3794882cc81da0d3d32ef38fc958f56173e35
otito-dev <otitofranklin70@gmail.com>
2026-02-01 20:03:53 +0100

    fixed security

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://Mortal:otitoboss555@cluster0.pdaevco.mongodb.net/PROJECT').then(()=>console.log("DB Connected"));
+   await mongoose.connect(process.env.MONGODB_URI)
+   .then(()=>console.log("DB Connected"))
+   .catch((err)=>console.log("DB Error:", err));
 }
